9225b5926dc84b1b1d7401e8287b253247e51c7a
Abdullah <abdullahpsg@gmail.com>
2024-06-16 17:05:25 +0700

    [NEW] create product view

--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,5 +8,27 @@
 </head>
 <body>
 	<h1>Create a product</h1>
+	<form method="POST" action="">
+		<div>
+			<label>Name</label>
+			<input type="text" name="name" placeholder="Name"/>
+		</div>
+		<div>
+			<label>Quantity</label>
+			<input type="text" name="qty" placeholder="Quantity"/>
+		</div>
+		<div>
+			<label>Price</label>
+			<input type="text" name="price" placeholder="Price"/>
+		</div>
+		<div>
+			<label>Description</label>
+			<input type="text" name="description" placeholder="Description"/>
+		</div>
+
+		<div>
+			<input type="submit" value="Save A New product" />
+		</div>
+	</form>
 </body>
 </html>
