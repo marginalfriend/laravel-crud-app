9521904d61cba1ddc6b44cb5cd58db5bf1a177b0
Abdullah <abdullahpsg@gmail.com>
2024-06-16 18:30:34 +0700

    [NEW] all products view

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -8,5 +8,25 @@
 </head>
 <body>
 	<h1>Product</h1>
+	<div>
+		<table border="1">
+			<tr>
+				<th>ID</th>
+				<th>Name</th>
+				<th>Price</th>
+				<th>Qty</th>
+				<th>Description</th>
+			</tr>
+			@foreach ($products as $product)
+				<tr>
+					<td>{{$product -> id}}</td>
+					<td>{{$product -> name}}</td>
+					<td>{{$product -> price}}</td>
+					<td>{{$product -> qty}}</td>
+					<td>{{$product -> description}}</td>
+				</tr>
+			@endforeach
+		</table>
+	</div>
 </body>
 </html>
