c1f24d8b57731a6efcc8e1123a9f26bdc3ee1e75
Abdullah <abdullahpsg@gmail.com>
2024-06-16 17:32:01 +0700

    [NEW] database persistance and error handling

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Product;
 
 class ProductController extends Controller
 {
@@ -12,5 +13,18 @@ class ProductController extends Controller
 
 	public function create() {
 		return view('products.create');
+	}
+
+	public function store(Request $request) {
+		$data = $request->validate([
+			'name'=>'required',
+			'qty'=>'required|numeric',
+			'price'=>'required|decimal:2',
+			'description'=>'nullable',
+		]);
+
+		$newProduct = Product::create($data);
+
+		return redirect(route('products.index'));
 	}
 }
--- resources/views/products/create.blade.php
+++ resources/views/products/create.blade.php
@@ -8,7 +8,21 @@
 </head>
 <body>
 	<h1>Create a product</h1>
-	<form method="POST" action="">
+	<div>
+		@if ($errors->any())
+		<ul>
+			@foreach ($errors->all() as $error)
+				<li>
+					{{$error}}
+				</li>
+			@endforeach
+		</ul>
+		@endif
+	</div>
+	
+	<form method="POST" action="{{route('products.store')}}">
+		@csrf
+		@method('POST')
 		<div>
 			<label>Name</label>
 			<input type="text" name="name" placeholder="Name"/>
